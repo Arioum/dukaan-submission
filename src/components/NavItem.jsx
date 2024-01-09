26c4f143fe1b1d3ee7b2bfad2a1cf90de650fafe
arioum <amoghrao121@gmail.com>
2024-01-10 00:51:30 +0530

    fixed opacity

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -1,6 +1,6 @@
 const NavItem = ({ navItem }) => {
   return (
-    <article className='flex items-center gap-[12px] text-white/80 px-[16px] py-[8px] rounded-[4px] hover:bg-white/10 hover:cursor-pointer group'>
+    <article className='flex items-center gap-[12px]  px-[16px] py-[8px] rounded-[4px] hover:bg-white/10 hover:cursor-pointer group'>
       <div className='w-[20px] h-[20px] flex justify-center items-center'>
         <img
           src={navItem.img}
@@ -8,7 +8,7 @@ const NavItem = ({ navItem }) => {
           className='opacity-80 group-hover:opacity-100'
         />
       </div>
-      <p className='text-[14px] leading-[20px] font-[500] opacity-80 group-hover:opacity-100'>
+      <p className='text-[14px] leading-[20px] font-[500] text-white opacity-80 group-hover:opacity-100'>
         {navItem.name}
       </p>
     </article>
