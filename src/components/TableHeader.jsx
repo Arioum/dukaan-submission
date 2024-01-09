9505f8b47f851da5b9dd1c54ecf5a64b7169e97f
arioum <amoghrao121@gmail.com>
2024-01-10 00:47:01 +0530

    fixed header radius

--- src/components/TableHeader.jsx
+++ src/components/TableHeader.jsx
@@ -3,7 +3,7 @@ import { BsCaretDownFill } from 'react-icons/bs';
 
 const TableHeader = () => {
   return (
-    <tr className='bg-[#F2F2F2] text-[14px] leading-[20px] text-[#4D4D4D] px-[10px] py-[12px] grid gap-[40px] grid-cols-4'>
+    <tr className='bg-[#F2F2F2] text-[14px] leading-[20px] text-[#4D4D4D] px-[10px] py-[12px] grid gap-[40px] grid-cols-4 rounded-[4px]'>
       <th className='flex grow font-[500]'>Order ID</th>
       <th className='flex grow font-[500] flex gap-[4px] items-center'>
         Order Date
