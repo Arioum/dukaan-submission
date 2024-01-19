c20a058951cb65b5da7ecfd53deecc6835f470f2
arioum <amoghrao121@gmail.com>
2024-01-19 19:59:59 +0530

    fixed main view centering issue

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -9,7 +9,7 @@ const Dashboard = () => {
       <SideBar />
       <div className='bg-[#FAFAFA] h-[100vh]'>
         <SearchBar />
-        <main className='w-[1152px] relative top-[24px] left-[calc(256px-224px)] flex flex-col gap-[32px] overflow-y-auto h-[calc(100%-64px-48px)] overflow-hidden hide-scroll'>
+        <main className='max-w-[1152px] mx-auto relative top-[24px] flex flex-col gap-[32px] overflow-y-auto h-[calc(100%-64px-48px)] overflow-hidden hide-scroll'>
           <OverView />
           <Transaction />
         </main>
